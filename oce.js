d21b91b8c026ef6bbd12980f36ef4bc07f018650
Bruno Calado <brunocalado@users.noreply.github.com>
2022-03-10 18:34:06 -0300

    1.2

--- oce.js
+++ oce.js
@@ -1,53 +1,41 @@
-class swffgUIModule {
-
-  constructor() {
-    // Random id to prevent collision with other modules;
-    this.ID = randomID(24);
-  }
-
-  async init() {
-    game.settings.register('old-crt-effects', 'flickering', {
-      name: game.i18n.localize('SWFFG.flickering'),
-      hint: game.i18n.localize('SWFFG.flickeringHint'),
-      scope: 'client',
-      type: Boolean,
-      default: false,
-      config: true,
-      onChange: () => {
-        location.reload();
-      },
-    });
-    game.settings.register('old-crt-effects', 'screenDoor', {
-      name: game.i18n.localize('SWFFG.screenDoor'),
-      hint: game.i18n.localize('SWFFG.screenDoorHint'),
-      scope: 'client',
-      type: Boolean,
-      default: false,
-      config: true,
-      onChange: () => {
-        location.reload();
-      },
-    });
-    game.settings.register('old-crt-effects', 'scanline', {
-      name: game.i18n.localize('SWFFG.scanline'),
-      hint: game.i18n.localize('SWFFG.scanlineHint'),
-      scope: 'client',
-      type: Boolean,
-      default: true,
-      config: true,
-      onChange: () => {
-        location.reload();
-      },
-    });
-
-
-    //here
-  }
-}
-
-Hooks.on("ready", () => {
-  swffgUIModule.singleton = new swffgUIModule();
-  swffgUIModule.singleton.init();
+Hooks.once('ready', function() {
+     
+  game.settings.register('old-crt-effects', 'flickering', {
+    name: game.i18n.localize('OCE.flickering'),
+    hint: game.i18n.localize('OCE.flickeringHint'),
+    scope: 'client',
+    type: Boolean,
+    default: false,
+    config: true,
+    onChange: () => {
+      location.reload();
+    },
+  });
+  
+  game.settings.register('old-crt-effects', 'screenDoor', {
+    name: game.i18n.localize('OCE.screenDoor'),
+    hint: game.i18n.localize('OCE.screenDoorHint'),
+    scope: 'client',
+    type: Boolean,
+    default: false,
+    config: true,
+    onChange: () => {
+      location.reload();
+    },
+  });  
+  
+  game.settings.register('old-crt-effects', 'scanline', {
+    name: game.i18n.localize('OCE.scanline'),
+    hint: game.i18n.localize('OCE.scanlineHint'),
+    scope: 'client',
+    type: Boolean,
+    default: true,
+    config: true,
+    onChange: () => {
+      location.reload();
+    },
+  });
+  
 });
 
 Hooks.once('ready', function() {
